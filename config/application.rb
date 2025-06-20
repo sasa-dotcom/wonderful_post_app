362340fd5e82cb41ac258ddfabad3e62e5dbf245
SATOMI <>
2025-06-20 21:28:11 +0900

    日本語

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,10 @@ module WonderfulPostApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    #
+    config.i18n.default_locale = :ja
+    #( デフォルトのlocaleを日本語(:ja)にする )
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
   end
 end
